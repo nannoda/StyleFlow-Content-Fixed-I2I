39e690fd9b63dfc9ea065151a112f9a6b4742cb0
Big Boy <bigboy@nannoda.com>
2025-03-17 13:57:00 -0500

    Update types

--- model/utils/sampler.py
+++ model/utils/sampler.py
@@ -1,12 +1,17 @@
 import math
+from typing import Optional
 import numpy as np
 from torch.utils.data.sampler import Sampler
 from collections import defaultdict
 
-
-
-class DistributedGivenIterationSampler(Sampler):
-    def __init__(self, dataset, total_iter, batch_size, world_size=None, rank=None, last_iter=-1):
+class DistributedGivenIterationSampler(Sampler[int]):
+    def __init__(self, 
+                 dataset: np.ndarray,  # Assuming dataset is a numpy array (or can be a list)
+                 total_iter: int,
+                 batch_size: int,
+                 world_size: Optional[int] = None,
+                 rank: Optional[int] = None,
+                 last_iter: int = -1):
         self.dataset = dataset
         self.total_iter = total_iter
         self.batch_size = batch_size
@@ -19,37 +24,42 @@ class DistributedGivenIterationSampler(Sampler):
         self.indices = self.gen_new_list()
         self.call = 0
 
-    def __iter__(self):
+
+    def __iter__(self) -> iter:
         if self.call == 0:
             self.call = 1
-            return iter(self.indices[(self.last_iter+1)*self.batch_size:])
+            return iter(self.indices[(self.last_iter + 1) * self.batch_size:])
         else:
             raise RuntimeError("this sampler is not designed to be called more than once!!")
 
-    def gen_new_list(self):
-
-        # each process shuffle all list with same seed, and pick one piece according to rank
+    def gen_new_list(self) -> np.ndarray:
+        """
+        Generates a shuffled list of indices for the given dataset.
+        Each process shuffles the list with the same seed and selects a piece according to the rank.
+        :return: The list of indices to sample from
+        """
+        # Set the seed for reproducibility
         np.random.seed(0)
 
         all_size = self.total_size * self.world_size
         indices = np.arange(len(self.dataset))
         indices = indices[:all_size]
-        num_repeat = (all_size-1) // indices.shape[0] + 1
+        num_repeat = (all_size - 1) // indices.shape[0] + 1
         indices = np.tile(indices, num_repeat)
         indices = indices[:all_size]
 
         np.random.shuffle(indices)
         beg = self.total_size * self.rank
-        indices = indices[beg:beg+self.total_size]
-        #print('len: ',len(indices))
-        #print('total: ',self.total_size)
+        indices = indices[beg:beg + self.total_size]
         assert len(indices) == self.total_size
 
         return indices
 
-    def __len__(self):
-        # note here we do not take last iter into consideration, since __len__
-        # should only be used for displaying, the correct remaining size is
-        # handled by dataloader
-        #return self.total_size - (self.last_iter+1)*self.batch_size
+
+    def __len__(self) -> int:
+        """
+        Returns the total number of elements this sampler will iterate over.
+        This method should not take into account the `last_iter` value as it is meant for display purposes.
+        :return: The total size of the dataset for this sampler
+        """
         return self.total_size
